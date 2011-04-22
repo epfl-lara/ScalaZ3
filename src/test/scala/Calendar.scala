fa422ddb46b190a2e95dcb7af26a495971a032ed
Ali Sinan Koksal <alisinan@gmail.com>
2011-04-22 02:04:39 +0200

    implicit conversion from scala primitives to Trees and calendar example with function

--- src/test/scala/Calendar.scala
+++ src/test/scala/Calendar.scala
@@ -10,21 +10,15 @@ class Calendar extends FunSuite with ShouldMatchers {
     val totalDays = 10593
     val originYear = 1980
 
-    val (year, day, _) = choose((year: Val[Int], day: Val[Int], leapDays: Val[Int]) =>
-      leapDays === (year - 1) / 4 - (year - 1) / 100 + (year - 1) / 400 - ((originYear - 1) / 4 - (originYear - 1) / 100 + (originYear - 1) / 400)
-      && totalDays === (year - originYear) * 365 + leapDays + day
-      && day > 0 && day <= 366)
+    val (year, day) = choose((year: Val[Int], day: Val[Int]) => {
+      def leapDaysUntil(y : Tree[IntSort]) = (y - 1) / 4 - (y - 1) / 100 + (y - 1) / 400
+
+      totalDays === (year - originYear) * 365 + leapDaysUntil(year) - leapDaysUntil(originYear) + day && 
+      day > 0 && day <= 366
+    })
 
     year should equal (2008)
     day should equal (366)
-
-    // val (year, days, years, leapYears) = choose((year: Val[Int], days: Val[Int], years: Val[Int], leapYears: Val[Int]) =>
-    //   leapYears === ((year - 1) / 4) - ((year - 1) / 100) + ((year - 1) / 400) - (1979/4 - 1979/100 + 1979/400) &&
-    //   totalDays === days + years * 365 + leapYears &&
-    //   years === year - 1980 &&
-    //   days >= 0 &&
-    //   days <= 366)
-
   }
 }
 

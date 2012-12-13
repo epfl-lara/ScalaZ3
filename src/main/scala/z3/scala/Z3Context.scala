59f718757df366cc1ae0721594e1ffb534cd2f8d
Etienne Kneuss <colder@php.net>
2012-12-13 16:02:42 +0100

    512 is too many solvers

--- src/main/scala/z3/scala/Z3Context.scala
+++ src/main/scala/z3/scala/Z3Context.scala
@@ -22,7 +22,7 @@ sealed class Z3Context(val config: Z3Config) {
   val astQueue       = new Z3RefCountQueue[Z3ASTLike]()
   val astvectorQueue = new Z3RefCountQueue[Z3ASTVector]()
   val modelQueue     = new Z3RefCountQueue[Z3Model]()
-  val solverQueue    = new Z3RefCountQueue[Z3Solver](512)
+  val solverQueue    = new Z3RefCountQueue[Z3Solver](5)
   val tacticQueue    = new Z3RefCountQueue[Z3Tactic]()
 
   def this(params : (String,Any)*) = this(new Z3Config(params : _*))

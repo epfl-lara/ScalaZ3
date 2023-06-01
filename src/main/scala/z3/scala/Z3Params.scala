ec078fc28e6e671a19a4fb530965e2cc56740732
Mario Bucev <mario.bucev@epfl.ch>
2023-06-01 10:38:56 +0200

    Allow to set unsigned int parameter

--- src/main/scala/z3/scala/Z3Params.scala
+++ src/main/scala/z3/scala/Z3Params.scala
@@ -8,6 +8,7 @@ private[scala] class Z3Params(val ptr: Long, val context: Z3Context) extends Z3O
   def set(key: String, value: Any): Unit = value match {
     case b: Boolean => Native.paramsSetBool(context.ptr, ptr, context.mkSymbol(key).ptr, b)
     case d: Double => Native.paramsSetDouble(context.ptr, ptr, context.mkSymbol(key).ptr, d)
+    case i: Int => Native.paramsSetUint(context.ptr, ptr, context.mkSymbol(key).ptr, i)
     case s: String => Native.paramsSetSymbol(context.ptr, ptr, context.mkSymbol(key).ptr, context.mkSymbol(s).ptr)
     case _ => throw new Z3Exception("Can't set value " + value + " of type " + value.getClass)
   }
